141955ea84b1c7c3e85082b3f13d90fc928f8c8f
Vincent Roseberry <rosbo@google.com>
2019-06-11 04:32:04 +0000

    fix pandas tests caused by read_feather incompatibility

--- tests/test_pandas.py
+++ tests/test_pandas.py
@@ -1,6 +1,7 @@
 import unittest
 
 import pandas as pd
+from pyarrow.feather import read_feather
 
 class TestPandas(unittest.TestCase):    
     def test_read_csv(self):
@@ -9,6 +10,12 @@ class TestPandas(unittest.TestCase):
         self.assertEqual(100, len(data.index))
 
     def test_read_feather(self):
-        data = pd.read_feather("/input/tests/data/feather-0_3_1.feather")
+        # Python 3.7 requires PyArrow >= 0.11.0
+        # pandas.read_feather(...) support for PyArrow >= 0.11.0 has been added in pandas 0.24 
+        # But tsfresh requires pandas<=0.23.4
+        # pyarrow.feather.read_feather should be used instead.
+        # TODO(rosbo): the twosigmanews lib will need to be updated.
+        # data = pd.read_feather("/input/tests/data/feather-0_3_1.feather")
+        data = read_feather("/input/tests/data/feather-0_3_1.feather")
 
         self.assertEqual(10, data.size)
